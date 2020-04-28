789efabe2f3939241ffea465151c6d3d161dd06f
moe <moe9999@protonmail.com>
2020-04-28 08:54:03 +0100

    Channel extension for a descriptor

--- src/main/kotlin/me/moe/logbot/extensions/ChannelExtensions.kt
+++ src/main/kotlin/me/moe/logbot/extensions/ChannelExtensions.kt
@@ -2,4 +2,4 @@ package me.moe.logbot.extensions
 
 import net.dv8tion.jda.api.entities.TextChannel
 
-fun TextChannel.descriptor() = "${this.asMention} :: ${this.name}"
+fun TextChannel.descriptor() = "**${this.asMention} :: ${this.name} :: ID :: ${this.id}**"
