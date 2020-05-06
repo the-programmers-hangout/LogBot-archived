9ee3d98bf13fa52ce0b770b39fada76fb0ebc312
moe <moe9999@protonmail.com>
2020-05-06 20:37:57 +0100

    Refactor docker to root of project

--- docker/scripts/deploy.bat
+++
@@ -1,3 +0,0 @@
-cd ../..
-docker build -t logbot:latest -f ./Docker/Dockerfile --no-cache .
-docker run -e BOT_TOKEN=%1 -v %2:/config logbot:latest
--- docker/scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../..
-docker build -t logbot:latest -f ./Docker/Dockerfile --no-cache .
-cmd="docker run -e BOT_TOKEN='$1' -v $2:/config logbot:latest"
-eval $cmd
